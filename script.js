b48b9ee9e9068f33cc8d4ff5e842ab3b34364039
Amber Forrest <amber4est@gmail.com>
2025-08-07 16:10:08 -0400

    Adding some DOM elements to script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const fetchButton = document.getElementbyId("fetchButton");
+const xhrButton = document.getElementbyId("xhrButton");
+const postForm = document.getElementbyId("postForm");
+const createPost = document.getElementbyId("createPost");
+const dataDisplay = document.getElementbyId("dataDisplay");
